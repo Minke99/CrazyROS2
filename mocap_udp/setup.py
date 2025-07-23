838c108cabedf3c272594870068894f5819ce5c9
lsongcityu <lsong22-c@my.cityu.edu.hk>
2025-07-23 11:24:39 +0800

    fix the build warning in setup.py

--- mocap_udp/setup.py
+++ mocap_udp/setup.py
@@ -7,6 +7,9 @@ setup(
     version='0.0.1',
     packages=[package_name],
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/mocap_launch.py']),
         ('share/' + package_name + '/config', ['config/mocap_params.yaml']),
     ],
@@ -22,4 +25,3 @@ setup(
         ],
     },
 )
-
